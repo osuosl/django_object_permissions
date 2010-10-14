bf15a398963709d7e65958ce07df1273cff9f931
Peter Krenesky <peter@osuosl.org>
2010-10-14 07:56:19 -0700

    adding url mapping for media packaged with object_permissions

--- object_permissions/urls.py
+++ object_permissions/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ganeti_webmgr.object_permissions.views.user_groups',
@@ -8,4 +10,11 @@ urlpatterns = patterns('ganeti_webmgr.object_permissions.views.user_groups',
     url(r'^user_group/(?P<id>\d+)/user/add/?$','add_user', name="user_group-add-user"),
     url(r'^user_group/(?P<id>\d+)/user/remove/?$','remove_user', name="user_group-remove-user"),
     url(r'^user_group/(?P<id>\d+)/user/$','user_permissions', name="user_group-user-permissions"),
+)
+
+#The following is used to serve up local media files like images
+root = '%s/media' % os.path.dirname(os.path.realpath(__file__))
+urlpatterns += patterns('',
+    (r'^object_permissions_media/(?P<path>.*)', 'django.views.static.serve',\
+     {'document_root':  root}),
 )
