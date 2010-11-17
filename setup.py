2de1b957d17b4262e6597d72cd050df936b9abfe
Corbin Simpson <simpsoco@osuosl.org>
2010-11-16 17:08:22 -0800

    setup: Change release number to 0.9.
    Had a change of heart.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 long_desc = open('README').read()
 
 setup(name='django-object-permissions',
-      version="1.0",
+      version="0.9",
       description='A method for adding object-level or row-level permissions',
       long_description=long_desc,
       author='Peter Krenesky',
