aad01ddf6d84b688c31ea93d16cd8e0690cdd98a
Corbin Simpson <simpsoco@osuosl.org>
2010-11-23 15:14:44 -0800

    backend: Rewrite with querysets for accuracy.
    More code, but improved accuracy, speed, etc.

--- object_permissions/backend.py
+++ object_permissions/backend.py
@@ -1,7 +1,8 @@
 from django.conf import settings
+from django.db import models
 from django.contrib.auth.models import User
 
-from object_permissions.registration import get_user_perms, user_has_perm
+from object_permissions.registration import permission_map, user_has_perm
 
 class ObjectPermBackend(object):
     supports_object_permissions = True
@@ -40,6 +41,8 @@ class ObjectPermBackend(object):
     def get_all_permissions(self, user_obj, obj=None):
         """
         Get a list of all permissions for the user on the given object.
+
+        This includes permissions given through groups.
         """
 
         if not user_obj.is_authenticated():
@@ -51,4 +54,46 @@ class ObjectPermBackend(object):
         if obj is None:
             return []
 
-        return get_user_perms(user_obj, obj)
+        model = obj.__class__
+        permissions = permission_map[model]
+
+        q = permissions.objects.filter(
+            models.Q(group__user=user_obj) | models.Q(user=user_obj),
+            obj=obj)
+
+        # Extract all set permissions from each row in the query.
+        rv = set()
+        for row in q:
+            rv.update(field.name for field in row._meta.fields
+                if isinstance(field, models.BooleanField)
+                and getattr(row, field.name))
+
+        return list(rv)
+
+    def get_group_permissions(self, user_obj, obj=None):
+        """
+        Get a list of permissions for this user's groups on the given object.
+        """
+
+        if not user_obj.is_authenticated():
+            if self.anonymous:
+                user_obj = self.anonymous
+            else:
+                return []
+
+        if obj is None:
+            return []
+
+        model = obj.__class__
+        permissions = permission_map[model]
+
+        q = permissions.objects.filter(groups__user=user_obj, obj=obj)
+
+        # Extract all set permissions from each row in the query.
+        rv = set()
+        for row in q:
+            rv.update(field.name for field in row._meta.fields
+                if isinstance(field, models.BooleanField)
+                and getattr(row, field.name))
+
+        return list(rv)
