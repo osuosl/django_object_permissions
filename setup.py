62555acfb8f5629dd4242d0885b0c23c6d305797
Peter Krenesky <peter@osuosl.org>
2011-02-25 20:28:43 -0800

    adding core project files:  CHANGELOG, LICENSE, and such

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+long_desc = open('README').read()
+
+setup(name='django-object-log',
+      version="0.5",
+      description='A method for logging user actions on models',
+      long_description=long_desc,
+      author='Peter Krenesky',
+      author_email='peter@osuosl.org',
+      maintainer="Peter Krenesky",
+      maintainer_email="peter@osuosl.org",
+      url='http://code.osuosl.org/projects/object-log',
+      packages=['object_log'],
+      include_package_data=True,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          'Framework :: Django',
+          'Topic :: Logging',
+          ],
+      )
