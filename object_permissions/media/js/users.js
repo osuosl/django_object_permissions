4260774c51cb0cffc2dd91e2d469090a97045b29
Peter Krenesky <peter@osuosl.org>
2011-04-26 18:36:14 -0700

    Ticket #4137 - Deleting user permissions with the delete button doesn't work:     ajax delete was sending empty array for "permissions" instead of nothing

--- object_permissions/media/js/users.js
+++ object_permissions/media/js/users.js
@@ -1,6 +1,9 @@
 /**
  * Javascript for object user's view
  */
+var user_url;
+var obj_id;
+
 $(document).ready(function() {
     // unbind all functions, this ensures that they are never bound more
     // than once.  This is a problem when using jquery ajax tabs
@@ -40,14 +43,14 @@ $(document).ready(function() {
     
     // Delete user button
     $('.user .delete').live("click", function() {
-        name = $(this).parent().parent().children('.name').html();
+        var name = $(this).parent().parent().children('.name').html();
         if (confirm("Remove this user: " + name)) {
             $('.qtip').qtip('destroy');
-            id = this.parentNode.parentNode.id.substring(5);
-            data = {user:id, permissions:[], obj:obj_id};
+            var id = this.parentNode.parentNode.id.substring(5);
+            var data = {user:id, obj:obj_id};
             $.post(user_url, data,
                 function(code){
-                    type = typeof code
+                    var type = typeof code;
                     if (type=="string") {
                         $("#user_" + id).remove();
                     }
@@ -58,13 +61,13 @@ $(document).ready(function() {
     
     // Delete group button
     $('.group .delete').live("click", function() {
-        name = $(this).parent().parent().children('.name').html();
+        var name = $(this).parent().parent().children('.name').html();
         if (confirm("Remove this group: "+ name)) {
-            id = this.parentNode.parentNode.id.substring(6);
-            data = {group:id, permissions:[], obj:obj_id};
+            var id = this.parentNode.parentNode.id.substring(6);
+            var data = {group:id, permissions:[], obj:obj_id};
             $.post(user_url, data,
                 function(code){
-                    type = typeof code
+                    var type = typeof code;
                     if (type=="string") {
                         $("#group_" + id).remove();
                     }
@@ -97,25 +100,24 @@ $(document).ready(function() {
 
 function update_user_permissions(responseText, statusText, xhr, $form) {
     if (xhr.getResponseHeader('Content-Type') == 'application/json') {
-        type = typeof responseText;
+        var type = typeof responseText;
         if (type == 'string') {
             // 1 code means success but no more permissions
             $('.qtip').qtip('hide');
             $("#op_users #" + responseText).remove();
         } else {
             // parse errors
-            errors = responseText
-            for (key in errors) {
-                $("#errors").append("<li>"+ errors[key] +"</li>")
+            for (var key in responseText) {
+                $("#errors").append("<li>"+ errors[key] +"</li>");
             }
         }
     } else {
         // successful permissions change.  replace the user row with the
         // newly rendered html
         $('.qtip').qtip('hide');
-        html = $(responseText);
-        id = html.attr('id')
-        $row = $('#op_users #' + id);
+        var html = $(responseText);
+        var id = html.attr('id');
+        var $row = $('#op_users #' + id);
         if ($row.length == 1) {
             $row.replaceWith(html);
         } else {
