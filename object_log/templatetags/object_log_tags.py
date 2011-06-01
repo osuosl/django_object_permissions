4b3c4b3978f70a64d9be275275b4155f41edce88
Peter Krenesky <peter@osuosl.org>
2011-06-01 09:24:38 -0700

    adding permalink tag for rendering links to objects when available

--- object_log/templatetags/object_log_tags.py
+++ object_log/templatetags/object_log_tags.py
@@ -9,3 +9,17 @@ def render_context(log_item, context):
     helper tag needed for adding extra context when rendering a LogItem
     """
     return SafeString(log_item.render(**context))
+
+
+@register.simple_tag
+def permalink(obj, display=None):
+    """
+    Return a link for an object if it as a get_absolute_url method.  Not all
+    models will have this.  Models that do not have the method will be rendered
+    as text
+    """
+    display = display if display else obj
+    if hasattr(obj, 'get_absolute_url'):
+        return '<a href="%s">%s</a>' % (obj.get_absolute_url(), display)
+    else:
+        return obj
