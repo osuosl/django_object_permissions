82a36644e47176f6258abdc8143469e967185c26
Jonah Brooks <brookjon@osuosl.org>
2011-07-22 16:36:07 -0700

    Modification to the user searchbox scripts to allow their use on other sections of the site.

--- object_permissions/views/widgets.py
+++ object_permissions/views/widgets.py
@@ -10,9 +10,45 @@ def search_users(request):
 
     term = request.GET['query']
     limit = 10
-    data = simplejson.dumps(search_users_and_groups(term, limit))
+    if request.GET.get("groups", 'True') == 'True':
+        data = simplejson.dumps(search_users_and_groups(term, limit))
+    else:
+        data = simplejson.dumps(search_users_only(term, limit))
     return HttpResponse(data, mimetype="application/json")
 
+def search_users_only(term=None, limit=10):
+    """
+    Returns a list of the top N matches from Users with a name
+    starting with term
+ 
+    @param term: the term to search for
+    @param limit: the number of results to return
+    """
+     
+    if term:
+        users = User.objects.filter(username__istartswith=term)
+    else:
+        users = User.objects.all()
+     
+    users = users.values('pk', 'username')
+     
+    if limit: 
+        users = users[:limit]
+     
+    # format list better for the interface
+    f = 'user'
+    users = [(i['username'], (f,i['pk'])) for i in users]
+     
+    # sort, trim, and unzip list into suggestions/data (if needed)
+    users = sorted(users, key=lambda x: x[0]) 
+    users = users if len(users) < limit else users[:limit]
+    suggestions, data = zip(*users) if users else ((),())
+ 
+    return {
+        'query':term,
+        'suggestions':suggestions,
+        'data':data
+    }
 
 def search_users_and_groups(term=None, limit=10):
     """
