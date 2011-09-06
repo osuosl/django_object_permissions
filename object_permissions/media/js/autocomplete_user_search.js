fa04909469f0f824fcd676490abba3225550f670
Jonah Brooks <brookjon@osuosl.org>
2011-09-06 13:33:25 -0700

    Updated autocomplete_user_search.js to better handle labels and tooltips.
    Added code to make the new search box take on the id of the one it replaces
    It now also moves any labels that reference the replaced search field into the
    span so that it can reference the new search field instead.
    
    Finally, the comments were updated to be more useful and informative.

--- object_permissions/media/js/autocomplete_user_search.js
+++ object_permissions/media/js/autocomplete_user_search.js
@@ -1,12 +1,25 @@
-//* search_box is the jquery object to replace with the search box, and the default handler
-//* search_url is the url of the search function
-//* handlers is an object of the format {'userTypeToHandle':$('jqueryObjectToHandleThatType'),etcetc}
+//* autocomplete_user_search creates an autocompleting search box for selecting users or groups
+//* It replaces one or more existing selection fields, importing various attributes from the originals
+//*
+//*     Arguments:
+//* search_box is the jquery object that will be replaced by the new search box
+//* search_url is a url that returns search suggestions given the user-given search tem
+//* handlers is an object of the format {'userTypeToHandle':$('jqueryObjectToHandleThatType'),etc:etc}
+//*     and is used in the event that the search needs to encompass more than one existing selector field
+//*     where each listed jquery object will be used when selecting a user of the corresponding type
 
 function autocomplete_user_search(search_box, search_url, handlers) {
     
+    function deselectOption()
+    {   $("#selector").removeClass("user").removeClass("group").removeClass("other");
+        search_box.val(0);
+        for(type in handlers)
+        {   handlers[type].val(0);
+        }
+    } 
+
     function selectOption(value, type)
-    {   
-        $("#selector").removeClass("user").removeClass("group").removeClass("other");
+    {   deselectOption(); 
         $("#selector").addClass(type);
         $("#selector input").val(value);
         
@@ -17,21 +30,22 @@ function autocomplete_user_search(search_box, search_url, handlers) {
         {   search_box.children("option:contains('"+value+"')").attr("selected", "selected");
         }
     }
-
-    function deselectOption()
-    {   $("#selector").removeClass("user").removeClass("group").removeClass("other");
-        search_box.val(0);
-        for(type in handlers)
-        {   handlers[type].val(0);
-        }
-    } 
     
     // Hide the primary search box, leaving its parent visible
-    // Then hide the parent element with class equal to each handler type
-    search_box.hide().parent().append("<span id=\"selector\"><input type=\"text\" name=\""+search_box.attr("name")+"_\"/></span>");
+    // Then create the autocompleting search box using the old search box's attributes
+    search_box.hide().parent().append("<span id=\"selector\"><input type=\"text\"/></span>");
+    $("#selector input").attr("id",search_box.attr("id")).attr("name",search_box.attr("name")+"_");
+
+    // Move the label, if any, inside the span so that it can apply to the new search box 
+    $("#selector").prepend($("label[for=\""+search_box.attr("id")+"\"]"));
+    search_box.attr("id", search_box.attr("id")+"-hidden"); 
+
+    // Hide the parent container for each secondary search box that the new box is replacing
     for(type in handlers)
     {   handlers[type].parents("."+type).hide();
     }
+
+    // Create, initialize, and manage the new autocompleting search field
     if( search_box.val() > 0 )
     {   selectOption( search_box.find("option:selected").text(), "other" );
     }
