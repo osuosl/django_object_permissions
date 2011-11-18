a75d8be6493d8e09f237659c826502aab9232a5e
Corbin Simpson <simpsoco@osuosl.org>
2011-11-17 16:27:13 -0800

    Bump to 0.7.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 long_desc = open('README').read()
 
 setup(name='django-object-log',
-      version="0.6",
+      version="0.7",
       description='A method for logging user actions on models',
       long_description=long_desc,
       author='Peter Krenesky',
       author_email='peter@osuosl.org',
-      maintainer="Peter Krenesky",
-      maintainer_email="peter@osuosl.org",
+      maintainer="Corbin Simpson",
+      maintainer_email="simpsoco@osuosl.org",
       url='http://code.osuosl.org/projects/django-object-log',
       packages=['object_log'],
       include_package_data=True,
