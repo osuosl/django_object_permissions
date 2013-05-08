75b3c7d2a746fd8307bc13c6d014209f75eaa24c
Trevor Bramwell <bramwelt@osuosl.org>
2013-05-08 13:30:37 -0700

    Removing TESTING variable
     * Removed TESTING from object_permissions/model.py
     * Moved test models from object_permissions/models.py to test/models.py
     * Added an import to tests for tests/models.py
    
    Refs #13515

--- object_permissions/registration.py
+++ object_permissions/registration.py
@@ -11,8 +11,6 @@ from django.db.models import Model, Q, Sum
 from object_permissions.signals import granted, revoked
 
 
-TESTING = settings.TESTING if hasattr(settings, 'TESTING') else False
-
 
 """
 Registration functions.
@@ -211,44 +209,6 @@ def _register_delayed(**kwargs):
 
 
 models.signals.post_syncdb.connect(_register_delayed)
-
-
-if TESTING:
-    # XXX Create test tables only when TEST mode.  These models will be used in
-    # various unittests.  This is used so that we do add unneeded models in a
-    # production deployment.
-    from django.db import models
-    class TestModel(models.Model):
-        name = models.CharField(max_length=32)
-    class TestModelChild(models.Model):
-        parent = models.ForeignKey(TestModel, null=True)
-    class TestModelChildChild(models.Model):
-        parent = models.ForeignKey(TestModelChild, null=True)
-        
-    TEST_MODEL_PARAMS = {
-        'perms' : {
-            # perm with both params
-            'Perm1': {
-                'description':'The first permission',
-                'label':'Perm One'
-            },
-            # perm with only description
-            'Perm2': {
-                'description':'The second permission',
-            },
-            # perm with only label
-            'Perm3': {
-                'label':'Perm Three'
-            },
-            # perm with no params
-            'Perm4': {}
-        },
-        'url':'test_model-detail',
-        'url-params':['name']
-    }
-    register(TEST_MODEL_PARAMS, TestModel, 'object_permissions')
-    register(['Perm1', 'Perm2','Perm3','Perm4'], TestModelChild, 'object_permissions')
-    register(['Perm1', 'Perm2','Perm3','Perm4'], TestModelChildChild, 'object_permissions')
 
 
 def get_class(class_name):
--- object_permissions/tests/__init__.py
+++ object_permissions/tests/__init__.py
@@ -2,3 +2,4 @@ from backend import *
 from permissions import *
 from groups import *
 from signals import *
+from models import *
